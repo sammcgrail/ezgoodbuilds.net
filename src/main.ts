ec892875ebd12631babd8ab172f9c63f7cd89a48
Sam <sammcgrail@gmail.com>
2021-03-22 15:32:36 -0400

    boiler fire

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,34 @@
 import { squared } from "./util";
+import * as tf from '@tensorflow/tfjs';
 
 console.log(squared(3));
+
+const a = tf.tensor(
+  [
+    [
+      [0, 1, 2],
+      [1, 2, 3],
+      [3, 4, 5]
+    ],
+    [
+      [0, 1, 2],
+      [1, 2, 3],
+      [3, 4, 5]
+    ]
+  ]);
+
+const b = tf.tensor(
+  [
+    [
+      [0, 1, 2],
+      [1, 2, 3],
+      [3, 4, 5]
+    ],
+    [
+      [0, 1, 2],
+      [1, 2, 3],
+      [3, 4, 5]
+    ]
+  ]);
+
+a.matMul(b).print();
