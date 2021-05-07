bf639345bb836de16ac7db59c01037ab579bf1cd
McWenker <mcdickers@DESKTOP-IO6AHCO.localdomain>
2021-05-07 16:35:46 -0400

    add image gallery, restrict upload to image

--- src/scripts/upload.js
+++ src/scripts/upload.js
@@ -1,27 +1,52 @@
 import app, { storage } from "../../.firebase/config.js"
 
-// Initialize Firebase
 function uploadImage() {
-  const ref = storage.ref()
-  const file = document.querySelector("#photo").files[0]
-  const name = new Date() + '-' + file.name
+  const ref = storage.ref();
+  const file = document.querySelector("#photo").files[0];
 
-  const metadata = {
-    contentType: file.type
+  if (file.type.match('image.*')) {
+    const name = new Date() + '-' + file.name;
+
+    const metadata = {
+      contentType: file.type
+    }
+
+    const task = ref.child(name).put(file, metadata);
+    task
+      .then(snapshot => snapshot.ref.getDownloadURL())
+      .then(url => {
+        console.log(url);
+        alert("image upload successful");
+        const image = document.querySelector('#image');
+        image.src = url;
+      });
   }
-
-  const task = ref.child(name).put(file, metadata)
-  task
-    .then(snapshot => snapshot.ref.getDownloadURL())
-    .then(url => {
-      console.log(url)
-      alert("image upload successful")
-      const image = document.querySelector('#image')
-      image.src = url
-    })
+  else {
+    file = null;
+    alert("please choose an image nob");
+  }
 }
 
+function displayImages() {
+  const ref = storage.ref();
+  ref.listAll()
+    .then(function (res) {
+      res.items.forEach((imageRef) => {
+        imageRef.getDownloadURL().then((url) => {
+          var imgCard = document.createElement('img');
+          imgCard.className = 'galleryCard';
+          imgCard.src = url;
+          document.getElementById('gallery').appendChild(imgCard);
+        });
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
 window.onload = function () {
+  displayImages();
   var btn = document.getElementById("uploadButton");
   btn.onclick = uploadImage;
 }
